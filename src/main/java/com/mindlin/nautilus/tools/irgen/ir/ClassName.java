9693b99dfed0f9e73d3a0176e0d421a991e54c25
mailmindlin <liamttlyrox@gmail.com>
2019-05-03 14:57:35 -0400

    Map ClassName from TypeElement

--- src/main/java/com/mindlin/nautilus/tools/irgen/ir/ClassName.java
+++ src/main/java/com/mindlin/nautilus/tools/irgen/ir/ClassName.java
@@ -2,7 +2,12 @@ package com.mindlin.nautilus.tools.irgen.ir;
 
 import java.util.Objects;
 
-public class ClassName extends TypeName {
+import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.SimpleElementVisitor8;
+
+public class ClassName extends TypeName implements Named {
 	public static ClassName get(Class<?> clazz) {
 		String name = clazz.getSimpleName();
 		
@@ -13,6 +18,11 @@ public class ClassName extends TypeName {
 		}
 		
 		return ClassName.get(clazz.getEnclosingClass()).nestedClass(name);
+	}
+	
+	public static ClassName get(TypeElement element) {
+		String name = element.getSimpleName().toString();
+		return element.getEnclosingElement().accept(new ElementMapper(), name);
 	}
 	
 	protected final String packageName;
@@ -41,6 +51,12 @@ public class ClassName extends TypeName {
 		return simpleName;
 	}
 	
+	@Override
+	public String getName() {
+		return this.getSimpleName();
+	}
+	
+	@Override
 	public String getReflectionName() {
 		if (this.enclosingClass != null)
 			return String.format("%s$%s", this.enclosingClass.getReflectionName(), this.simpleName);
@@ -67,7 +83,7 @@ public class ClassName extends TypeName {
 	
 	@Override
 	public String toString() {
-		return this.getReflectionName();
+		return this.getQualifiedName();
 	}
 	
 	@Override
@@ -90,5 +106,30 @@ public class ClassName extends TypeName {
 		return Objects.equals(this.getPackageName(), c2.getPackageName())
 				&& Objects.equals(this.getEnclosingClass(), c2.getEnclosingClass())
 				&& Objects.equals(this.getSimpleName(), c2.getSimpleName());
+	}
+	
+	protected static class ElementMapper extends SimpleElementVisitor8<ClassName, String> {
+		@Override
+		public ClassName visitPackage(PackageElement packageElement, String simpleName) {
+			return new ClassName(packageElement.getQualifiedName().toString(), null, simpleName);
+		}
+		
+		@Override
+		public ClassName visitType(TypeElement enclosingClass, String simpleName) {
+			String esn = enclosingClass.getSimpleName().toString();
+			return enclosingClass.getEnclosingElement()
+					.accept(this, esn)
+					.nestedClass(simpleName);
+		}
+		
+		@Override
+		public ClassName visitUnknown(Element e, String simpleName) {
+			return new ClassName("", simpleName);
+		}
+		
+		@Override
+		protected ClassName defaultAction(Element e, String simpleName) {
+			throw new IllegalArgumentException("Unexpected type");
+		}
 	}
 }
