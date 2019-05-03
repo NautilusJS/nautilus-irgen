34ba7ebd614277cc31f7953be50d3a0457fa6f36
mailmindlin <liamttlyrox@gmail.com>
2019-05-02 23:45:29 -0400

    Prefer types over raw strings

--- src/main/java/com/mindlin/nautilus/tools/irgen/ir/ParameterSpec.java
+++ src/main/java/com/mindlin/nautilus/tools/irgen/ir/ParameterSpec.java
@@ -2,39 +2,43 @@ package com.mindlin.nautilus.tools.irgen.ir;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 
 import com.mindlin.nautilus.tools.irgen.Utils.Writable;
 
-public class ParameterSpec implements Writable {
-	protected boolean isFinal;
-	protected String type;
-	protected boolean varargs;
-	protected String name;
+public class ParameterSpec implements Writable, Named {
+	protected final boolean isFinal;
+	protected final TypeName type;
+	protected final boolean varargs;
+	protected final String name;
 	
-	public ParameterSpec(String type, String name) {
+	public ParameterSpec(TypeName type, String name) {
 		this(false, type, false, name);
 	}
 	
-	public ParameterSpec(boolean isFinal, String type, String name) {
+	public ParameterSpec(boolean isFinal, TypeName type, String name) {
 		this(isFinal, type, false, name);
 	}
 	
-	public ParameterSpec(String type, boolean varargs, String name) {
+	public ParameterSpec(TypeName type, boolean varargs, String name) {
 		this(false, type, varargs, name);
 	}
 	
-	public ParameterSpec(boolean isFinal, String type, boolean varargs, String name) {
+	public ParameterSpec(boolean isFinal, TypeName type, boolean varargs, String name) {
 		this.isFinal = isFinal;
 		this.type = type;
 		this.varargs = varargs;
 		this.name = name;
 	}
 	
-	public String getType() {
+	public ParameterSpec withName(String name) {
+		return new ParameterSpec(this.isFinal, this.type, this.varargs, name);
+	}
+	
+	public TypeName getType() {
 		return this.type;
 	}
 	
+	@Override
 	public String getName() {
 		return this.name;
 	}
@@ -42,8 +46,8 @@ public class ParameterSpec implements Writable {
 	@Override
 	public String toString() {
 		StringWriter sw = new StringWriter();
-		try {
-			write(sw);
+		try (CodeWriter cw = new CodeWriter(sw)){
+			write(cw);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -51,17 +55,13 @@ public class ParameterSpec implements Writable {
 	}
 	
 	@Override
-	public void write(Writer out) throws IOException {
+	public void write(CodeWriter out) throws IOException {
 		if (this.isFinal)
 			out.append("final ");
 		
-		out.append(getType());
-		
 		if (this.varargs)
-			out.append("...");
+			out.emit("$T...$N", this.getType(), this);
 		else
-			out.append(" ");
-		
-		out.append(getName());
+			out.emit("$T $N", this.getType(), this);
 	}
 }
