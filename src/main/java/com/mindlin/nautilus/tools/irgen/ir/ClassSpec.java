525ac5be0636dd91900efe600a02a7f39d5d3275
mailmindlin <liamttlyrox@gmail.com>
2019-05-20 20:32:42 -0400

    Write better import blocks

--- src/main/java/com/mindlin/nautilus/tools/irgen/ir/ClassSpec.java
+++ src/main/java/com/mindlin/nautilus/tools/irgen/ir/ClassSpec.java
@@ -2,8 +2,12 @@ package com.mindlin.nautilus.tools.irgen.ir;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,6 +16,62 @@ import javax.lang.model.element.Element;
 import javax.tools.JavaFileObject;
 
 public abstract class ClassSpec {
+	protected static String getNamespace(String pkg) {
+		if (pkg == null)
+			return pkg;
+		int firstDot = pkg.indexOf('.');
+		if (firstDot >= 0)
+			return pkg.substring(0, firstDot);
+		return pkg;
+	}
+	
+	protected static int getNamespacePriority(String namespace) {
+		if (namespace == null)
+			return -1;
+		switch (namespace) {
+			case "java":
+				return 0;
+			case "javax":
+				return 1;
+			case "org":
+				return 2;
+			case "com":
+				return 3;
+			default:
+				return 4;
+		}
+	}
+	
+	protected static int comparePackageNames(String pkgA, String pkgB) {
+		if (pkgA == null)
+			return pkgB == null ? -1 : 0;
+		if (pkgB == null)
+			return 1;
+		
+		String namespaceA = getNamespace(pkgA);
+		String namespaceB = getNamespace(pkgB);
+		
+		int result = getNamespacePriority(namespaceA) - getNamespacePriority(namespaceB);
+		if (result != 0)
+			return result;
+		
+		result = pkgA.compareTo(pkgB);
+		
+		return result;
+	}
+	
+	protected static int compareImports(ClassName a, ClassName b) {
+		int result = ClassSpec.comparePackageNames(a.getPackageName(), b.getPackageName());
+		if (result != 0)
+			return result;
+		
+		if (a.getSimpleName() == null)
+			return b.getSimpleName() == null ? 0 : -1;
+		if (b.getSimpleName() == null)
+			return 1;
+		
+		return a.getSimpleName().compareTo(b.getSimpleName());
+	}
 
 	protected Element[] getSources() {
 		return new Element[0];
@@ -58,6 +118,39 @@ public abstract class ClassSpec {
 		}
 	}
 	
+	protected void writeImports(CodeWriter writer) {
+		List<ClassName> imports = new ArrayList<>(this.getImports());
+		if (imports.isEmpty())
+			return;
+		
+		imports.sort(ClassSpec::compareImports);
+		
+		Set<String> packageImports = new HashSet<>();
+		Set<String> names = new HashSet<>();
+		String lastNamespace = null;
+		for (ClassName ipclz : imports) {
+			if (ipclz.getEnclosingClass() == null && packageImports.contains(ipclz.getPackageName()))
+				continue;
+			if (names.contains(ipclz.getSimpleName()))
+				// Another import exists with the same name
+				continue;
+			
+			String namespace = getNamespace(ipclz.getPackageName());
+			if (lastNamespace != null && !Objects.equals(namespace, lastNamespace))
+				writer.println();
+			lastNamespace = namespace;
+			
+			writer.emitImport(ipclz);
+			writer.addImport(ipclz);
+			if (ipclz.getSimpleName() == null)
+				packageImports.add(ipclz.getPackageName());
+			else
+				names.add(ipclz.getName());
+		}
+		
+		writer.println();
+	}
+	
 	protected void writeHeritage(CodeWriter writer) throws IOException {
 		Optional<TypeName> parent = this.getSuper();
 		if (parent.isPresent())
@@ -73,6 +166,7 @@ public abstract class ClassSpec {
 	}
 	
 	public void write(Writer _writer) throws IOException {
+		@SuppressWarnings("resource")
 		final CodeWriter writer = new CodeWriter(_writer, "\t");
 		// Write package declaration
 		if (!this.getPackage().isEmpty()) {
@@ -80,13 +174,7 @@ public abstract class ClassSpec {
 			writer.setPackage(this.getPackage());
 		}
 		
-		Set<ClassName> imports = this.getImports();
-		for (ClassName ipclz : imports) {
-			writer.emitImport(ipclz);
-			writer.addImport(ipclz);
-		}
-		if (!imports.isEmpty())
-			writer.println();
+		this.writeImports(writer);
 		
 		// Emit a SuppressWarnings b/c possible unused imports
 		writer.println("@SuppressWarnings(\"unused\")");
